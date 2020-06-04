2a43f3c10ba0912e27b620fa34285351e892398d
jvalentina.velez <jvalentina.velez@udea.edu.co>
2020-06-03 21:06:53 -0500

    Validación Email

--- contactenos.js
+++ contactenos.js
@@ -17,3 +17,8 @@ function Contacto(event){
     contactos.push(contacto);
     console.log(contactos);
 }
+
+function validateEmail(email) { 
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+} 
